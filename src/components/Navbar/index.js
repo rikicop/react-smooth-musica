a0c7657521599301e01232b11084f688ed98d1ea
rikicop <ruperto1@protonmail.com>
2021-08-12 15:51:31 -0400

    titulo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -40,7 +40,7 @@ const Navbar = ({ toggle }) => {
             <IconContext.Provider value={{ color: '#fff' }}>
                 <Nav scrollNav={scrollNav}>
                     <NavbarContainer>
-                        <NavLogo to='/' onClick={toggleHome}>Tecnemas</NavLogo>
+                        <NavLogo to='/' onClick={toggleHome}>Escuela de Música</NavLogo>
                         <MobileIcon onClick={toggle}>
                             <FaBars />
                         </MobileIcon>
